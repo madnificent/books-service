b9bc1a99bebef1b6d38e23a7e313ad20b188059b
Aad Versteden <madnificent@gmail.com>
2016-05-02 22:35:53 +0200

    Initial import of configuration
    This configuration contains base /books and /authors resources.

---
+++ domain.lisp
@@ -0,0 +1,20 @@
+(in-package :mu-cl-resources)
+
+(define-resource book ()
+  :class (s-prefix "schema:Book")
+  :properties `((:title :string ,(s-prefix "schema:headline"))
+                (:isbn :string ,(s-prefix "schema:isbn")))
+  :has-many `((author :via ,(s-prefix "schema:author")
+                      :as "authors"))
+  :resource-base (s-url "http://mu.semte.ch/services/github/madnificent/book-service/books/")
+  :on-path "books")
+
+(define-resource author ()
+  :class (s-prefix "schema:Author")
+  :properties `((:name :string ,(s-prefix "schema:name")))
+  :has-many `((book :via (s-prefix "schema:author")
+                    :inverse t
+                    :as "books"))
+  :resource-base (s-url "http://mu.semte.ch/services/github/madnificent/book-service/authors/")
+  :on-path "authors")
+ 
---
+++ repository.lisp
@@ -0,0 +1,6 @@
+(in-package :mu-cl-resources)
+
+(add-prefix "mu" "http://mu.semte.ch/vocabularies/core/")
+(add-prefix "dcterms" "http://purl.org/dc/terms/")
+(add-prefix "schema" "http://schema.org/")
+
